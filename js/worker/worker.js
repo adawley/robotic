3470bd9ea6f8b3291ddc24b4cd5e597bcb8eddd0
Rob Dawley <adawley@gmail.com>
2013-01-25 22:56:23 -0500

    tried to implement xmlhttprequest in worker.js.

--- js/worker/worker.js
+++ js/worker/worker.js
@@ -1,37 +1,77 @@
 var i=0;
 var name = '';
 var weight = 0;
+var wires = new Array();
+var started = false;
+var xmlhttp = new XMLHttpRequest();
 
 self.addEventListener('message', function(e) {
-  var data = e.data;
-	switch (data.cmd) {
-		case 'name':
-			if(data.msg == ''){
-				self.postMessage('NAMED: ' + name);				
-			} else {
-				name = data.msg;
-			}			
-			break;
-		case 'start':
-			self.postMessage('WORKER STARTED: ' + data.msg);
-			break;
-		case 'stop':
-			self.postMessage('Worker: ' + name + ' over out.');
-			self.close(); // Terminates the worker.
-			break;
-		case 'timedCount':
-			i=i+1;
-			postMessage(i);
-			setTimeout("timedCount()",2000);
-			break;
-		case 'weight':
-			if(data.msg == ''){				
-				self.postMessage('Worker ' + name + ' weight: ' + weight);
-			} else {
-				weight = data.msg;
-			}
-			break;
-		default:
-			self.postMessage('Unknown command: ' + data.msg);
-	}
+    var data = e.data;
+    switch (data.cmd) {
+        case 'kill':
+            self.postMessage({
+                'cmd':'dead',
+                'msg':name
+            });
+            self.close();
+        case 'name':
+            if(data.msg == ''){
+                self.postMessage(name);
+            } else {
+                name = data.msg;
+            }			
+            break;
+        case 'start':
+            if(!started){
+                started = true;
+                timedCount();                
+            }
+            break;
+        case 'stop':
+            started = false;
+            break;
+        case 'weight':
+            if(data.msg == ''){				
+                self.postMessage('Worker ' + name + ' weight: ' + weight);
+            } else {
+                weight = data.msg;
+            }
+            break;
+        case 'wire':
+            wires.push(data.msg);
+            self.postMessage(wires);
+            break;
+        default:
+            self.postMessage('Unknown command: ' + data.msg);
+    }
 }, false);
+
+function timedCount(){
+    i=i+1;
+    
+    ajax("http://www.google.com/finance/info?infotype=infoquoteall&q=GOOG&callback=?",function(data){
+        self.postMessage(i+' - '+data);
+        if(started){
+            setTimeout("timedCount()",10000);
+        }
+    });   
+    
+    
+}
+
+
+function ajax(url, callback){
+    
+    xmlhttp.onreadystatechange=function()
+    {
+        postMessage(xmlhttp.readyState+':'+xmlhttp.status);
+        if (xmlhttp.readyState==4)
+        {
+            postMessage('ajax4');
+            callback(xmlhttp.responseText);
+        }
+    }
+    xmlhttp.open("GET",url,true);
+    xmlhttp.send();
+    
+}
